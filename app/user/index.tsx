f1930609bd0901aaaa801fbf7c96476f49d45b77
Quynh Chi Lai <quynhchi1009@yandex.com>
2024-02-21 19:15:09 +0100

    Add layout, index, settings, and user pages

---
+++ app/user/index.tsx
@@ -0,0 +1,31 @@
+/** @format */
+
+import { StyleSheet, Text, View } from "react-native";
+
+export default function User() {
+	return (
+		<View style={styles.container}>
+			<View style={styles.main}>
+				<Text style={styles.title}>User Page</Text>
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		padding: 24,
+	},
+	main: {
+		flex: 1,
+		justifyContent: "center",
+		maxWidth: 960,
+		marginHorizontal: "auto",
+	},
+	title: {
+		fontSize: 64,
+		fontWeight: "bold",
+	},
+});
