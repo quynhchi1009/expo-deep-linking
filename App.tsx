a8932917709342cd00d0c5864b8518be6a764725
Quynh Chi Lai <quynhchi1009@yandex.com>
2024-02-21 18:12:51 +0100

    Add expo-dev-client and deep linking support***
    ***Update app.json with scheme and EAS project ID***
    
    ***Update App.tsx to handle deep linking and display parsed URL

--- App.tsx
+++ App.tsx
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/** @format */
+
+import * as Linking from "expo-linking";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [data, setData] = useState<Linking.ParsedURL>();
+
+	function handleDeepLink(event: { url: string }) {
+		let data = Linking.parse(event.url);
+		setData(data);
+	}
+
+	useEffect(() => {
+		async function getInitialURL() {
+			const getInitialURL = await Linking.getInitialURL();
+			if (getInitialURL) {
+				let parsedUrl = Linking.parse(getInitialURL);
+				setData(parsedUrl);
+			}
+		}
+
+		const subscription = Linking.addEventListener("url", handleDeepLink);
+    Linking.addEventListener("url", handleDeepLink);
+    
+		if (!data) {
+			getInitialURL();
+    }
+
+		return () => {
+			subscription.remove();
+		};
+	}, []);
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text>
+				{data ? JSON.stringify(data) : "App wasn't opened from deep link!"}
+			</Text>
+			<StatusBar style='auto' />
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+		marginHorizontal: 20,
+	},
 });
