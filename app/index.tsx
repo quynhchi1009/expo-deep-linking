3d0d00af42b5a6e97ddaaa3a5879a33038a7c2e5
Quynh Chi Lai <quynhchi1009@yandex.com>
2024-02-22 11:57:15 +0100

    Update assets and package.json, add splash screen functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,51 @@
+/** @format */
+
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Page() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
-  );
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+	const [isReady, setReady] = useState(false);
+
+	useEffect(() => {
+		// Perform some sort of async data or asset fetching.
+		setTimeout(() => {
+			// When all loading is setup, unmount the splash screen component.
+			SplashScreen.hideAsync();
+			setReady(true);
+		}, 10000);
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.main}>
+				<Text style={styles.title}>Hello World</Text>
+				<Text style={styles.subtitle}>This is the first page of your app.</Text>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
+	container: {
+		flex: 1,
+		alignItems: "center",
+		padding: 24,
+	},
+	main: {
+		flex: 1,
+		justifyContent: "center",
+		maxWidth: 960,
+		marginHorizontal: "auto",
+	},
+	title: {
+		fontSize: 64,
+		fontWeight: "bold",
+	},
+	subtitle: {
+		fontSize: 36,
+		color: "#38434D",
+	},
 });
